571799c1348aab12893860fb97e0695df28e7c03
Ben Dunn <benthedunn@gmail.com>
2022-03-01 15:06:47 -0700

    validation for email

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,14 @@
 class Customer < ApplicationRecord
+  def email_valid()
+    unless email.include? '@'
+      errors.add(:email, 'Email must contain @')
+    end
+    unless email.include? '.'
+      errors.add(:email, 'Email must contain .')
+    end
+  end
+  
+  validates :email, presence: true, length: { minimum: 5 }
+  validate :email_valid
 end
+
